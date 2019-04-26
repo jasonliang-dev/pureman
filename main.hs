398a2ea7f3e4fc86f73c41abdcc041a4ff4f0f5d
summerjacket <dxmcxd@gmail.com>
2019-04-26 00:56:35 -0700

    picks random word from word file

--- main.hs
+++ main.hs
@@ -1,2 +1,7 @@
+import System.Random (randomRIO)
+
+pick :: [a] -> IO a
+pick xs = fmap (xs !!) $ randomRIO (0, length xs - 1)
+
 main :: IO ()
-main = print "Hello World!"
+main = fmap lines (readFile "words.txt") >>= pick >>= print
