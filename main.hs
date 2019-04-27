c9ba85d464b1704b2d9e655ee60192fb5d853baa
summerjacket <dxmcxd@gmail.com>
2019-04-26 19:34:54 -0700

    finished game

--- main.hs
+++ main.hs
@@ -1,7 +1,57 @@
 import System.Random (randomRIO)
+import qualified Data.Set as Set
+import Data.Char (toLower)
+import Control.Monad (when)
+
+data Model = 
+    Model 
+        { word :: String
+        , guesses :: Set.Set Char
+        } deriving (Show)
+
+data Msg = SetWord String | AddGuess Char
+
+initialModel :: Model
+initialModel = Model { word = "", guesses = Set.empty }
+
+update :: Msg -> Model -> Model
+update msg model =
+    case msg of
+        SetWord word -> 
+            model { word = word }
+        AddGuess letter ->
+            model { guesses = Set.insert letter $ guesses model }
+
+displayWord :: Set.Set Char -> String -> String
+displayWord guesses = foldl (\acc x -> acc ++ if toLower x `elem` guesses then [x] else "_") ""
+
+display :: Int -> Set.Set Char -> String -> String
+display maxGuesses guesses word = 
+    "Letters guessed:" ++ guessContents guesses ++ "\n\n"
+    ++ displayWord guesses word ++ "\n\n"
+    ++ show (maxGuesses - length guesses) ++ " guesses left"
+    where guessContents = foldl (\acc x -> acc ++ " " ++ [x]) ""
+
+maxGuesses :: Int
+maxGuesses = 10
+
+game :: Model -> IO ()
+game model = do
+    let xs = guesses model
+    let str = display maxGuesses xs (word model)
+    when ('_' `elem` str && length xs < maxGuesses) $ do
+        putStrLn str
+        putStr "Guess a letter: "
+        (letter:_) <- getLine
+        game $ update (AddGuess $ toLower letter) model
 
 pick :: [a] -> IO a
 pick xs = fmap (xs !!) $ randomRIO (0, length xs - 1)
 
 main :: IO ()
-main = fmap lines (readFile "words.txt") >>= pick >>= print
+main = do
+    text <- readFile "words.txt"
+    word <- pick $ lines text
+    print word
+    game $ update (SetWord word) initialModel
+    putStrLn $ "The word was " ++ word ++ "!"
